508222901c878efde5da68de51ca4ceb46db5ce7
Thomas W. <96929397+ctwyrth@users.noreply.github.com>
2024-10-04 07:46:49 -0700

    2nd - added data connection

--- src/app/bank.ts
+++ src/app/bank.ts
@@ -1,4 +1,5 @@
 export interface Bank {
   id: number;
   name: string;
+  website: string;
 }
--- src/app/banks.ts
+++ src/app/banks.ts
@@ -1,13 +1,13 @@
 import { Bank } from './bank';
 
 export const BANKS: Bank[] = [
-  { id: 11, name: "Bank of America Merrill Lynch" },
-  { id: 12, name: "Barclays Capital" },
-  { id: 13, name: "Citi" },
-  { id: 14, name: "Credit Suisse" },
-  { id: 15, name: "Deutsche Bank" },
-  { id: 16, name: "Goldman Sachs" },
-  { id: 17, name: "J.P. Morgan" },
-  { id: 18, name: "Morgan Stanley" },
-  { id: 19, name: "UBS" },
+  { id: 11, name: "Bank of America Merrill Lynch", website: "https://www.bankofamerica.com" },
+  { id: 12, name: "Barclays Capital", website: "https://www.ib.barclays" },
+  { id: 13, name: "Citi", website: "https://banking.citi.com" },
+  { id: 14, name: "Credit Suisse", website: "https://www.bankofamerica.com" },
+  { id: 15, name: "Deutsche Bank", website: "https://www.db.com" },
+  { id: 16, name: "Goldman Sachs", website: "https://www.goldmansachs.com" },
+  { id: 17, name: "J.P. Morgan", website: "https://www.jpmorgan.com" },
+  { id: 18, name: "Morgan Stanley", website: "https://www.morganstanley.com" },
+  { id: 19, name: "UBS", website: "https://www.ubs.com" },
 ];
